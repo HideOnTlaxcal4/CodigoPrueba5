408a4553562dc345909acc32427501cd54b5e693
eduardo <edu4yh@gmail.com>
2024-12-16 19:49:34 -0600

    Primer commit: Estructura Inicial

---
+++ Codigo5.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length(); // Longitud de haystack
+        int n = needle.length();   // Longitud de needle
+        
+        return -1; // Retorno temporal
+    }
+}
