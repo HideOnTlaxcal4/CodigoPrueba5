6a7e310e6e7fc61bfd6933cbc955aee17540e61e
eduardo <edu4yh@gmail.com>
2024-12-16 19:50:26 -0600

    Segundo commit: Se agrega bucle

--- Codigo5.java
+++ Codigo5.java
@@ -1,8 +1,12 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int m = haystack.length(); // Longitud de haystack
-        int n = needle.length();   // Longitud de needle
-        
+        int m = haystack.length();
+        int n = needle.length();
+
+        for (int i = 0; i <= m - n; i++) {
+            // Lógica de comparación se añadirá después
+        }
+
         return -1; // Retorno temporal
     }
 }
